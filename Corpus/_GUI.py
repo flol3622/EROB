23029fa1bfa9ec4bc2a5f5b5a353be4461d01b21
psoubrie <Philippe.Soubrier@UGent.be>
2024-05-14 19:53:43 +0200

    compile extra files

--- Corpus/_GUI.py
+++ Corpus/_GUI.py
@@ -1,6 +1,11 @@
+import os
 import sys
+
 from PyQt6 import QtWidgets, uic
-from PyQt6.QtWidgets import QTableWidgetItem, QComboBox
+from PyQt6.QtCore import QTimer
+from PyQt6.QtWidgets import QComboBox, QTableWidgetItem
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def genderComboBox():
@@ -12,6 +17,22 @@ def genderComboBox():
 def employmentComboBox():
     comboBox = QComboBox()
     comboBox.addItems(["FTE", "PTE", "Retired", "School", "Student", "Unemployed"])
+    return comboBox
+
+
+def occupancyComboBox():
+    comboBox = QComboBox()
+    comboBox.addItems(
+        [
+            "Nightshift",
+            "Early shift",
+            "Short absence",
+            "Dayshift",
+            "Half day absence",
+            "Long absence",
+            "Always present",
+        ]
+    )
     return comboBox
 
 
@@ -32,38 +53,57 @@ def bedroomComboBox(bedrooms):
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi("./Corpus/GUI/layout.ui", self)
+        uic.loadUi(os.path.join(base_dir, "GUI/layout.ui"), self)
         self.show()
 
         # accord person table to number of persons
         self.numPersons.valueChanged.connect(self.updatePeopleTable)
-        self.updatePeopleTableColumns()
 
+        # disable all columns
+        for i in range(1, 4):
+            self.peopleTable.setColumnHidden(i, True)
+
+        # if the checkbox is checked, show the column
+        self.genderBool.stateChanged.connect(
+            lambda: self.peopleTable.setColumnHidden(1, not self.genderBool.isChecked())
+        )
+        self.employmentBool.stateChanged.connect(
+            lambda: self.peopleTable.setColumnHidden(
+                2, not self.employmentBool.isChecked()
+            )
+        )
+        self.bedroomBool.stateChanged.connect(
+            lambda: self.peopleTable.setColumnHidden(
+                3, not self.bedroomBool.isChecked()
+            )
+        )
+
+        # connect the button to highlight the numPersons spinbox
+        self.goToNumPersons.clicked.connect(self.highlightNumPersons)
 
     def updatePeopleTable(self):
         rows = self.numPersons.value()
         self.peopleTable.setRowCount(rows)
         for i in range(rows):
+            # add label i inside the first column, unchangable (label)
+            self.peopleTable.setItem(i, 0, QTableWidgetItem(f"Person {i + 1}"))
             if self.peopleTable.cellWidget(i, 0) is None:
-                self.peopleTable.setCellWidget(i, 0, genderComboBox())
-                self.peopleTable.setCellWidget(i, 1, employmentComboBox())
-                self.peopleTable.setCellWidget(i, 2, bedroomComboBox(self.numBedrooms))
+                self.peopleTable.setCellWidget(i, 1, genderComboBox())
+                self.peopleTable.setCellWidget(i, 2, employmentComboBox())
+                self.peopleTable.setCellWidget(i, 3, bedroomComboBox(self.numBedrooms))
 
-    def updatePeopleTableColumns(self):
-        # disable all columns
-        for i in range(3):
-            self.peopleTable.setColumnHidden(i, True)
-        
-        # if genderBool is checked, unhide first column, keep listening to changes
-        self.genderBool.stateChanged.connect(
-            lambda: self.peopleTable.setColumnHidden(0, not self.genderBool.isChecked())
-        )
-        self.employmentBool.stateChanged.connect(
-            lambda: self.peopleTable.setColumnHidden(1, not self.employmentBool.isChecked())
-        )
-        self.bedroomBool.stateChanged.connect(
-            lambda: self.peopleTable.setColumnHidden(2, not self.bedroomBool.isChecked())
-        )
+        self.peopleTable2.setRowCount(rows)
+        for i in range(rows):
+            if self.peopleTable2.cellWidget(i, 0) is None:
+                for j in range(7):
+                    self.peopleTable2.setCellWidget(i, j, occupancyComboBox())
+
+    def highlightNumPersons(self):
+        self.tabWidget.setCurrentIndex(0)
+        self.label_14.setStyleSheet("border: 2px solid blue;")
+        self.timer = QTimer()
+        self.timer.timeout.connect(lambda: self.label_14.setStyleSheet("border: none;"))
+        self.timer.start(500)
 
 
 if __name__ == "__main__":
--- venv.md
+++ venv.md
@@ -6,11 +6,16 @@ conda activate erob
 
 # extremely fast package installer
 pip install uv 
-uv pip install pyqt6-tools PySide6
+uv pip install pyqt6-tools PySide6 auto-py-to-exe
 ```
 
 ## Qt Designer
 ```bash
 # open Qt Designer once in the environment
 pyqt6-tools designer .\Corpus\GUI\layout.ui
+```
+
+## Compile
+```bash
+pyinstaller --noconfirm --onefile --console --icon "C:/Users/phili/Documents/scratch/EROB/Corpus/GUI/icon.ico" --name "EROB" --add-data "C:/Users/phili/Documents/scratch/EROB/Corpus/GUI/layout.ui;GUI"  "C:/Users/phili/Documents/scratch/EROB/Corpus/_GUI.py"
 ```
